06c0e6b355787acc5e5de5a39aa74b1919be7844
edgar-treischl <edgar.treischl@fau.de>
2022-06-11 21:07:52 +0200

    init

--- inst/graphs/pacman.R
+++ inst/graphs/pacman.R
@@ -1,13 +1,8 @@
-#library(tidyverse)
-library(patchwork)
-library(showtext)
-library(ggplot2)
-
 #The Pac Man#########
 showplot <- function(variables) {
-  font_add_google("Press Start 2P", "Press Start 2P")
+  sysfonts::font_add_google("Press Start 2P", "Press Start 2P")
   ## Automatically use showtext to render text for future devices
-  showtext_auto()
+  showtext::showtext_auto()
 
   df<-tidyr::tribble(
     ~Charts, ~Amount,
@@ -17,28 +12,28 @@ showplot <- function(variables) {
 
 
   #Very reduce blank theme
-  blank_theme <- theme_minimal(base_size = 10)+
-    theme(
-      axis.title.x = element_blank(),
-      axis.title.y = element_blank(),
-      panel.border = element_blank(),
-      panel.grid=element_blank(),
-      axis.ticks = element_blank(),
-      axis.text = element_blank(),
-      plot.subtitle = element_text(size=12),
-      plot.title = element_text(size=16)
+  blank_theme <- ggplot2::theme_minimal(base_size = 10)+
+    ggplot2::theme(
+      axis.title.x = ggplot2::element_blank(),
+      axis.title.y = ggplot2::element_blank(),
+      panel.border = ggplot2::element_blank(),
+      panel.grid= ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      axis.text = ggplot2::element_blank(),
+      plot.subtitle = ggplot2::element_text(size=12),
+      plot.title = ggplot2::element_text(size=16)
     )
 
 
-  ggplot(df, aes(x="", y=Amount, fill=Charts))+
-    geom_bar(width = 60, stat = "identity", colour = "black")+
-    coord_polar("y", start=pi / 1.5)+
-    scale_fill_manual(values=c("white", "#ffff00")) +
+  ggplot2::ggplot(df, ggplot2::aes(x="", y=Amount, fill=Charts))+
+    ggplot2::geom_bar(width = 60, stat = "identity", colour = "black")+
+    ggplot2::coord_polar("y", start=pi / 1.5)+
+    ggplot2::scale_fill_manual(values=c("white", "#ffff00")) +
     blank_theme +
-    theme(legend.position="right")+
-    theme(text=element_text(family="Press Start 2P"))+
-    guides(fill = guide_legend(reverse=TRUE))+
-    labs(title = "Pie Charts and Pac-Man",
+    ggplot2::theme(legend.position="right")+
+    ggplot2::theme(text=ggplot2::element_text(family="Press Start 2P"))+
+    ggplot2::guides(fill = ggplot2::guide_legend(reverse=TRUE))+
+    ggplot2::labs(title = "Pie Charts and Pac-Man",
          subtitle = "How many resemble a Pac-Man?",
          fill = " ")
 
