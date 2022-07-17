3b0488b3606ea19cee93d57d85671a441755d361
edgar-treischl <edgar.treischl@fau.de>
2022-07-17 10:57:33 +0200

    font added long_wide

--- inst/graphs/long_wide.R
+++ inst/graphs/long_wide.R
@@ -1,31 +1,35 @@
-library(ggplot2)
+#library(ggplot2)
 library(patchwork)
 
 showplot <- function(variables) {
+  sysfonts::font_add_google("Patua One", "Patua")
+  showtext::showtext_auto()
   df_long <- expand.grid(x = 1:5, y = 1:4)
 
   labels_long <- c("2", "2", "1", "1", "ID",
-                   "2", "1" , "2",  "1", "t",
-                   "x2", "x1" , "x2",  "x1", "x",
-                   "y", "y" , "y",  "y", "y")
+                   "2", "1" , "2",  "1", "T",
+                   "x2", "x1" , "x2",  "x1", "X",
+                   "y", "y" , "y",  "y", "Y")
   colour_long <-  c("#74a9cf", "#74a9cf", "#045a8d", "#045a8d", "#302E2E",
                     "#74a9cf", "#74a9cf", "#045a8d", "#045a8d", "#302E2E",
                     "#74a9cf", "#74a9cf", "#045a8d", "#045a8d", "#302E2E",
                     "#d0d1e6", "#d0d1e6", "#d0d1e6", "#d0d1e6", "#302E2E")
 
-  p_long <- ggplot(df_long, aes(x=x, y=y)) +
-    geom_tile(color = "white", size = 1, fill = colour_long)+
-    geom_text(aes(label = labels_long), color = "white",
-              size = 5)+
-    coord_flip()+
-    theme_minimal()+
-    theme(panel.grid.minor = element_blank(),
-          panel.grid.major =  element_blank())+
-    theme(axis.title=element_blank(),
-          axis.text=element_blank(),
-          axis.ticks=element_blank())+
-    labs(title = "A: Long")+
-    theme(plot.title = element_text(face = "bold"))
+  p_long <- ggplot2::ggplot(df_long, ggplot2::aes(x=x, y=y)) +
+    ggplot2::geom_tile(color = "white", size = 1, fill = colour_long)+
+    ggplot2::geom_text(ggplot2::aes(label = labels_long), color = "white",
+              size = 5, family="Patua")+
+    ggplot2::coord_flip()+
+    ggplot2::theme_minimal()+
+    ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
+          panel.grid.major =  ggplot2::element_blank())+
+    ggplot2::theme(axis.title=ggplot2::element_blank(),
+          axis.text=ggplot2::element_blank(),
+          axis.ticks=ggplot2::element_blank())+
+    ggplot2::labs(title = "A: Long")+
+    ggplot2::theme(text=ggplot2::element_text(family="Patua",
+                                              face = "bold"))
+
 
 
 
@@ -33,26 +37,30 @@ showplot <- function(variables) {
   df_wide <- expand.grid(x = 1:3, y = 1:4)
 
   labels_wide <- c("2", "1", "ID",
-                   "x", "x", "x1",
-                   "x", "x", "x2",
-                   "y", "y", "y")
+                   "x", "x", "X1",
+                   "x", "x", "X2",
+                   "y", "y", "Y")
   colour_wide <-  c("#74a9cf", "#045a8d", "#302E2E",
                     "#74a9cf", "#045a8d", "#302E2E",
                     "#74a9cf", "#045a8d","#302E2E",
                     "#d0d1e6", "#d0d1e6", "#302E2E")
 
-  p_wide <- ggplot(df_wide, aes(x=x, y=y)) +
-    geom_tile(color = "white", size = 1, fill = colour_wide)+
-    geom_text(aes(label = labels_wide), color = "white", size = 5)+
-    coord_flip()+
-    theme_minimal()+
-    theme(panel.grid.minor = element_blank(),
-          panel.grid.major =  element_blank())+
-    theme(axis.title=element_blank(),
-          axis.text=element_blank(),
-          axis.ticks=element_blank())+
-    labs(title = "B: Wide")+
-    theme(plot.title = element_text(face = "bold"))
+  p_wide <- ggplot2::ggplot(df_wide, ggplot2::aes(x=x, y=y)) +
+    ggplot2::geom_tile(color = "white", size = 1, fill = colour_wide)+
+    ggplot2::geom_text(ggplot2::aes(label = labels_wide),
+              color = "white",
+              size = 5, family="Patua")+
+    ggplot2::coord_flip()+
+    ggplot2::theme_minimal()+
+    ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
+          panel.grid.major =  ggplot2::element_blank())+
+    ggplot2::theme(axis.title=ggplot2::element_blank(),
+          axis.text=ggplot2::element_blank(),
+          axis.ticks=ggplot2::element_blank())+
+    ggplot2::labs(title = "B: Wide")+
+    ggplot2::theme(plot.title = ggplot2::element_text(face = "bold"))+
+    ggplot2::theme(text=ggplot2::element_text(family="Patua",
+                                              face = "bold"))
 
   p_long + p_wide
 }
